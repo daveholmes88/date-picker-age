8c8c936b25494dba8b195e15e42f70ff400d306d
daveholmes88 <github email address>
2020-10-30 15:38:39 -0500

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import Calendar from 'react-calendar';
 import './App.css';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
+
+  const date = new Date()
+  const [age, setAge] = useState('')
+
+  const changeDate = newDate => {
+    const birthDate = newDate
+    let year = date.getFullYear() - birthDate.getFullYear()
+    let month = date.getMonth() - birthDate.getMonth()
+    let day = date.getDate() - birthDate.getDate()
+    if (month < 0) {
+      year -= 1
+      month += 12
+    }
+    if (month === 0 && day < 0) {
+      year -= 1
+      month += 11
+    }
+    setAge(`${year} years, ${month} months`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar
+        onChange={changeDate}
+        value={date}
+      />
+      <h3>{`Age: ${age}`}</h3>
     </div>
   );
 }
